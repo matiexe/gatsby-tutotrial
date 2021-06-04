e23ff0ce06fa82a2d56d009c4a753dd42dec72ba
mexxtz <matiexe31@gmail.com>
2021-06-04 14:02:14 -0300

    Se agregaron las paginas index y about, se agegrego el modulo link para refenciar las distintas paginas

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const AboutPage =() =>{
+    return(
+        <main>
+            <title>Aboute ME</title>
+            <h1>About me</h1>
+            <Link to ="/">Back to home</Link>
+            <p>Hi there! I' am the proud creaori of this site, wich I built with Gatsby</p>
+        </main>
+    )
+}
+
+export default AboutPage
