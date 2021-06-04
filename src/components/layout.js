dc3f44ed39ab60f9dd128efb09f4e4430503f1e1
mexxtz <matiexe31@gmail.com>
2021-06-04 15:05:42 -0300

    se agregan mas estilos

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,25 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import { container } from './layout.module.css'
+import { 
+    container,
+    heading,
+    navlinks,
+    navlinkItem,
+    navLinktext 
+
+} from './layout.module.css'
 
 const Layout = ({pageTitle, children}) =>{
     return(
         <main className = {container}>
             <title>{pageTitle}</title>
             <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
+                <ul className={navlinks}>
+                    <li className={navLinktext}><Link to="/">Home</Link></li>
+                    <li className={navLinktext}><Link to="/about">About</Link></li>
                 </ul>
             </nav>
-            <h1>{pageTitle}</h1>
+            <h1 className={heading}>{pageTitle}</h1>
             {children}
         </main>
     )
